417fe634c76b81b57c8e53cd56486074992233b7
shamimulhaque162 <khandoker15-1992@diu.edu.bd>
2022-10-21 20:24:01 +0600

    some logic changed

--- model/service.js
+++ model/service.js
@@ -18,13 +18,13 @@ exports.makeTour = async (data) => {
 }
 
 exports.findTourId = async (id) => {
-    const tour = await Tour.find({id});
+    const tour = await Tour.findTourId(id);
     return tour;
 }
 
 exports.updateTourId = async (id, data) => {
-    const tour = await Tour.find({id});
-    const result = await Tour.set.save();
+    const tour = await Tour.findTourId(id);
+    const result = await tour.set(data).save();
     return result;
 }
 
